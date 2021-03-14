6108b903b3dc638ee1d0176faf5019f802307f28
parknnna <af777888@naver.com>
2021-03-14 16:35:46 +0900

    포

--- chat-server.js
+++ chat-server.js
@@ -5,7 +5,7 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const portNo = 3000
+const portNo = 80
 server.listen(portNo, () => {
   console.log('서버 실행 완료:', 'http://localhost:' + portNo)
 })
